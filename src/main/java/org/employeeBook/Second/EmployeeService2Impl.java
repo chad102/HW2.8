f4ab52edc76c23f9db36e29f1a483c4e79e9028a
Чингис <cianeed08@gmail.com>
2023-12-12 00:43:27 +0300

    Create new Controller and Service

---
+++ src/main/java/org/employeeBook/Second/EmployeeService2Impl.java
@@ -0,0 +1,13 @@
+package org.employeeBook.Second;
+
+import org.employeeBook.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class EmployeeService2Impl {
+
+
+}
