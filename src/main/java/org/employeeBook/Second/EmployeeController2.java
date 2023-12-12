f65194e8e152631a7004a77a3516bf58665ff8f3
Чингис <cianeed08@gmail.com>
2023-12-12 10:21:00 +0300

    Create new Controller and Service

--- src/main/java/org/employeeBook/Second/EmployeeController2.java
+++ src/main/java/org/employeeBook/Second/EmployeeController2.java
@@ -6,13 +6,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/departments")
 public class EmployeeController2 {
-    private final EmployeeServiceImpl employeeServiceImpl;
-    public EmployeeController2(EmployeeServiceImpl employeeServiceImpl) {
-        this.employeeServiceImpl = employeeServiceImpl;
+    private final EmployeeService2Impl employeeService2Impl;
+    public EmployeeController2(EmployeeService2Impl employeeService2Impl) {
+        this.employeeService2Impl = employeeService2Impl;
     }
-
+@GetMapping(path = "/max-salary")
+    public Optional<Employee> getMaxSalaryInDep(@RequestParam("departmentId") int departmentId) {
+        return employeeService2Impl.getMaxSalaryInDep(departmentId);
     }
+@GetMapping(path = "/min-salary")
+    public Optional<Employee> getMinSalaryInDep(@RequestParam("departmentId") int departmentId) {
+        return employeeService2Impl.getMinSalaryInDep(departmentId);
+    }
+@GetMapping(path = "/all")
+    public List<Employee> printAllDep(@RequestParam("departmentId") int departmentId) {
+        return employeeService2Impl.printEmployeesInDep(departmentId);
+}
+@GetMapping(path = "/all")
+    public List<Employee> printEmployeesAllDep(@RequestParam(defaultValue = "departmentId") int departmentId) {
+        return employeeService2Impl.printEmployeesAllDep();
+}
 }
--- src/main/java/org/employeeBook/Second/EmployeeService2Impl.java
+++ src/main/java/org/employeeBook/Second/EmployeeService2Impl.java
@@ -8,6 +8,29 @@ import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService2Impl {
+    private final List<Employee> employees = new ArrayList<>();
+    public Optional<Employee> getMaxSalaryInDep (int departmentId) {
+        return employees.stream()
+                .filter(e -> e.getDepartmentId() == departmentId)
+                .max(Comparator.comparingInt(Employee::getSalary));
+    }
 
+    public Optional<Employee> getMinSalaryInDep (int departmentId) {
+        return employees.stream()
+                .filter(e -> e.getDepartmentId() == departmentId)
+                .min(Comparator.comparingInt(Employee::getSalary));
+    }
+
+    public List<Employee> printEmployeesInDep (int departmentId) {    /// ready?
+        return employees.stream()
+                .filter(e -> e.getDepartmentId() == departmentId)
+                .collect(Collectors.toList());
+    }
+
+    List<Employee> printEmployeesAllDep() {
+        return employees.stream()
+                .sorted(Comparator.comparingInt(Employee::getDepartmentId))
+                .collect(Collectors.toList());
+    }
 
 }
