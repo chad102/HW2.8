6338d8d537a7d299d252c223a3be71876e1c965d
Чингис <cianeed08@gmail.com>
2023-12-18 00:03:11 +0300

    Create new Controller and Service

--- src/main/java/org/employeeBook/Second/DepartmentController.java
+++ src/main/java/org/employeeBook/Second/DepartmentController.java
@@ -25,11 +25,11 @@ public class DepartmentController {
         return departmentServiceImpl.getMinSalaryInDep(departmentId);
     }
 @GetMapping(path = "/all")
-    public List<Employee> printAllDep(@RequestParam("departmentId") int departmentId) {
-        return departmentServiceImpl.printEmployeesInDep(departmentId);
-}
-@GetMapping(path = "/all")
-    public List<Employee> printEmployeesAllDep() {
-        return departmentServiceImpl.printEmployeesAllDep();
-}
+    public List<Employee> printAllDep(@RequestParam(defaultValue = "departmentId") String departmentId) {
+        if (departmentId.equals("departmentId")) {
+            return departmentServiceImpl.printEmployeesInDep(Integer.parseInt(departmentId));
+        } else {
+            return departmentServiceImpl.printEmployeesAllDep();
+        }
+    }
 }
